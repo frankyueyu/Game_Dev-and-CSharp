804e81b709406d64ac7be30262cafbd8bddd8ed9
Frank Yu <frank@yuyuan.org>
2026-08-07 16:26:57 -0700

    forgot to add previously

---
+++ InfiniteLoop.cs
@@ -0,0 +1,19 @@
+// DO NOT USE FORBIDDEN PROGRAM SCARY BAD
+// CANNOT DOES NOT WILL NOT
+// VERY VERY BAD REALLY BAD NONONO
+// DO NOT USE.
+
+
+using System;
+
+class StupidStuff
+{
+    public static void InfiniteLoop()
+    {
+        int i = 0;
+        while (true)
+        {
+            Console.Write(i);
+        }
+    }
+}
