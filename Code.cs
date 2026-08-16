7c6ce0a9c2c1acd0866fc487a7f40237613d91fa
Frank Yu <frank@yuyuan.org>
2026-08-15 19:37:30 -0700

    forgot to add

---
+++ Code.cs
@@ -0,0 +1,41 @@
+class Code
+{
+    public static string MorseToKeyboardSection(string morse, char[] keyboardLeft, char[] keyboardRight)
+    {
+        Random random = new Random();
+        string output = "";
+
+        foreach (char i in morse)
+        {
+            switch (i)
+            {
+                case '.':
+                {
+                    output += keyboardLeft[random.Next(keyboardLeft.Length)];
+                    break;
+                }
+                case '-':
+                {
+                    output += keyboardRight[random.Next(keyboardRight.Length)];
+                    break;
+                }
+                case ' ':
+                {
+                    output += " ";
+                    break;
+                }
+                case '/':
+                {
+                    output += " | ";
+                    break;
+                }
+                default:
+                {
+                    output += "?";
+                    break;
+                }
+            }
+        }
+        return output;
+    }
+}
