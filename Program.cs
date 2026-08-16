330a0405478f088db3ba23828576063ec0c8a816
Frank Yu <frank@yuyuan.org>
2026-08-16 13:51:22 -0700

    fixed semicolon

--- Program.cs
+++ Program.cs
@@ -52,7 +52,7 @@ class Program
             char[][] arrayChars = [[.. arrayLetters[0].Select(s => s[0])],
                             [.. arrayLetters[0].Select(s => s[0])]];
             
-            Console.WriteLine("Enter morse: ")
+            Console.WriteLine("Enter morse: ");
             Code.MorseToKeyboardSection(Console.ReadLine(), arrayChars[0], arrayChars[1]);
         }
     }
